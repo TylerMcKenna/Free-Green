5558ff86fe7603def08e8e406446a9f54dae5783
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-07 22:16:50 -0600

    Finished Project

--- push.sh
+++ push.sh
@@ -1,21 +1,11 @@
 if [ -e "./fakeWork.txt" ]; then
     rm ./fakeWork.txt
-    git add .
-    git commit -m ""
-    git push origin main
 else 
     touch ./fakeWork.txt
 fi 
 
-echo "Enter your message"
+echo "Enter your commit message"
 read message
 git add .
 git commit -m"${message}"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "IT IS CLEAN"
-else
- git status
- echo "Pushing data to remote server!!!"
- git push -u origin master
-fi
+git push -u origin master
