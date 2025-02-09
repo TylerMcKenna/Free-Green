3375b8867f494b88f6b95c7ad8282879ab2b0470
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-09 12:18:07 -0600

    This commit is a certified test

--- push.sh
+++ push.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-if [ -e "/home/jimmybob/Desktop/Free-Green/fakeWork.txt" ]; then
-    rm /home/jimmybob/Desktop/Free-Green/fakeWork.txt
-else 
-    touch /home/jimmybob/Desktop/Free-Green/fakeWork.txt
-fi 
-(cd /home/jimmybob/Desktop/Free-Green; git add .)
-(cd /home/jimmybob/Desktop/Free-Green; git commit -m "This commit is a certified test")
-(cd /home/jimmybob/Desktop/Free-Green; git push origin main)
+# directoryPath="test"
+
+lastFileModificationDate=`stat --format="%y" /home/jimmybob/Desktop/Free-Green/fakeWork.txt`
+currentDate=`date +"%F"`
+
+echo ${lastFileModificationDate:0:10}
+echo $currentDate
+
+if [[ ${lastFileModification:0:10} != $currentDate ]]; then 
+    if [ -e "/home/jimmybob/Desktop/Free-Green/fakeWork.txt" ]; then
+        echo "Work" > /home/jimmybob/Desktop/Free-Green/fakeWork.txt
+    else 
+        touch /home/jimmybob/Desktop/Free-Green/fakeWork.txt
+    fi 
+    (cd /home/jimmybob/Desktop/Free-Green; git add .)
+    (cd /home/jimmybob/Desktop/Free-Green; git commit -m "This commit is a certified test")
+    (cd /home/jimmybob/Desktop/Free-Green; git push origin main)
+else
+    echo "No need to commit twice in a day."
+fi
