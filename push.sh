0933dcb8d230f676d4fe261e2680a551a200c403
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-08 23:53:01 -0600

    This commit is a certified test

--- push.sh
+++ push.sh
@@ -6,5 +6,5 @@ else
     touch /home/jimmybob/Desktop/Free-Green/fakeWork.txt
 fi 
 (cd /home/jimmybob/Desktop/Free-Green; git add .)
-(cd /home/jimmybob/Desktop/Free-Green; git commit -m "This commit is a certified banger")
+(cd /home/jimmybob/Desktop/Free-Green; git commit -m "This commit is a certified test")
 (cd /home/jimmybob/Desktop/Free-Green; git push origin main)
